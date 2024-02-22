240065eeee4f740376b7dd0d00c4c4d9aeb9fb62
sridharu346 <sridharu346@gmail.com>
2024-02-22 15:41:21 -0500

    Add Java files to main

---
+++ hello.java
@@ -0,0 +1,17 @@
+class hello {
+    public static void main(String a[]) {
+        int n = 5;
+        int result ;
+
+        if(n%2==0)
+            result = 10;
+        else
+            result = 20;
+        
+        System.out.println(result);
+
+
+
+    }
+    
+}
